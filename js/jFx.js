d57437585d73a0d0df00e3ea258810d755329e78
Vladimir Mihailovic <vladimir.mihailovic@gmail.com>
2017-11-03 16:16:04 +0100

    style updates

---
+++ js/jFx.js
@@ -0,0 +1,23 @@
+$(function() {
+    var $inputField = $( ".np-input-field" );
+
+    $inputField.each(function (e) {
+        if ($(this).find("input").val()) {
+            $(this).addClass('active');
+        }
+    });
+
+    $inputField.on( "focusin", "input", function() {
+        $(this).parent().addClass('active');
+    });
+
+    $inputField.on( "focusout", "input", function() {
+        if (!$(this).val()) {
+            $(this).parent().removeClass('active');
+        }
+    });
+
+    $inputField.on( "click", ".reset", function() {
+        $(this).parent().find("input").val('').focus();
+    });
+});
