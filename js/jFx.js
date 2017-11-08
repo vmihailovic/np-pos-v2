ed0a8fbb467fc3ff39a6b7cc9f5eec21354fa256
Vladimir Mihailovic <vladimir.mihailovic@gmail.com>
2017-11-08 18:38:38 +0100

    style updates

--- js/jFx.js
+++ js/jFx.js
@@ -1,6 +1,5 @@
 $(function() {
     var $inputField = $( ".np-form" );
-    var $npItem = $('.np-item');
 
     $inputField.each(function () {
         if ($(this).find("input").val()) {
@@ -28,17 +27,53 @@ $(function() {
     });
 
 
+
+    var $npItem = $('.np-item');
+
     $npItem.each(function () {
         $(this).find('.edit-mode').css('transform', 'translateY(' + -$(this).height() + 'px)');
     });
 
     $npItem.on( "click", function() {
-        $('.np-item').removeClass('active');
+        $npItem.removeClass('active');
         $('.not-relevant').css('margin-top', 0);
         $(this).addClass('active');
 
         if($(this).hasClass('active')) {
-            $(this).find('.not-relevant').css('margin-top', -$(this).find('.not-relevant').height());
+            $(this).find('.not-relevant').css('margin-top', -$(this).find('.not-relevant').height()-1); // -1 for IE fix
         }
     });
+
+
+
+
+    var $npNavigation = $('.np-navigation');
+    var $navActive = $npNavigation.find('.active');
+    var $indicator = $npNavigation.append('<div class="indicator"></div>').find('.indicator');
+
+    var $right = function(el) {
+        return $npNavigation.width() - ( el.position().left + el.outerWidth() );
+    };
+
+    var $left = function(el) {
+        return el.position().left;
+    };
+
+    $indicator.css({
+        'left' : $left($navActive),
+        'right' : $right($navActive)
+    });
+
+    $npNavigation.find('div').on( "click", function() {
+        $npNavigation.find('div').removeClass('active');
+        $(this).addClass('active');
+
+        $indicator.velocity({
+            "right": $right($npNavigation.find('.active')) }, { duration: 300, queue: false, easing: 'easeOutQuad'
+        });
+        $indicator.velocity({
+            "left": $left($npNavigation.find('.active')) }, {duration: 300, queue: false, easing: 'easeOutQuad', delay: 90
+        });
+    });
+
 });
