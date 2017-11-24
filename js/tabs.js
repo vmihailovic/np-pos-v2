3521ab37a7db8fc62b4b171c581ae7a4e7e605ec
Vladimir Mihailovic <vladimir.mihailovic@gmail.com>
2017-11-24 14:55:03 +0100

    style updates

--- js/tabs.js
+++ js/tabs.js
@@ -1,7 +1,6 @@
 function setTabs(link, el) {
     $(link).on('click', function(e){
         var index = $(this).index();
-        $(el + '> div').removeClass('active');
         $(el + '> div:nth-child(' + (index+1) + ')').css('transform', 'translate3d(0, 0, 0)').nextAll().css('transform', 'translate3d(100%, 0, 0)');
         $(el + '> div:nth-child(' + (index+1) + ')').prevAll().css('transform', 'translate3d(-100%, 0, 0)');
     });
